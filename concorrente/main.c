ea98a0e21eca14d35c13ca7d50697fa60c69d229
leonardoCatharin <leo_catharin@hotmail.com>
2017-02-01 21:31:58 -0200

    Faltando apenas um caixa atender do outro

--- concorrente/main.c
+++ concorrente/main.c
@@ -1,80 +1,107 @@
-/*Código paralelo*/
-
-//Bibliotecas
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <time.h>
+#include "stdio.h"
+#include "stdlib.h"
 #include "mpi.h"
+#include <time.h>
 
-#define NUMERO_CLIENTES 10
+#define TAG_TAMANHO_FILA_PEDIDO 50
+#define TAG_TAMANHO_FILA_RESPOSTA 51
+#define TAG_ATENDIMENTO 51
+#define NUMERO_MAX_CLIENTES 20
 #define NUMERO_CAIXAS 5
-#define TAG_CAIXA_RCV 50
-#define TAG_CAIXA_SND 51
-#define TAG_SEND_FILA 52
+int numero_cliente = 0;
 
-int RANK_TO_RECEIVE;
+void atende_cliente(int rank) {
+    printf("Cliente atendido no caixa %i\n", rank);
+}
 
-void produtor() {
-    int caixas_fila[NUMERO_CAIXAS],
-            numero_cliente = 0,
-            valor = 1,
-            ack = 0,
-            soma_filas = 1;
+int cria_cliente() {
+    
+    int numero_c =  1 + ( rand() % 5 );
+    if(NUMERO_MAX_CLIENTES - numero_cliente <= 10){
+        numero_c =  1;
+    }
+    if(numero_c != 1){
+        while((numero_c + numero_cliente) > NUMERO_MAX_CLIENTES){
+            numero_c = 1 + ( rand() % 5 );
+        }
+    }
+    if (numero_cliente == NUMERO_MAX_CLIENTES) {
+        return -1;
+    }
+    
+    numero_cliente+= numero_c;
+    return numero_c;
+}
 
-    int j;
-    for (j = 0; j < NUMERO_CAIXAS; j++) {
-        caixas_fila[j] = 0;
+int menor_fila() {
+    int i;
+    int menor = 1;
+    int tamanho_filas[NUMERO_CAIXAS];
+    int pedido_tamanho_fila;
+
+    for (i = 1; i < NUMERO_CAIXAS; i++) {
+        MPI_Send(&pedido_tamanho_fila, 1, MPI_INT, i, TAG_TAMANHO_FILA_PEDIDO, MPI_COMM_WORLD);
     }
 
-    int indexMenorFila = 0;
-    while (numero_cliente < NUMERO_CLIENTES) {
-        int i, flag = 0;
+    for (i = 1; i < NUMERO_CAIXAS; i++) {
+        MPI_Recv(&tamanho_filas[i], 1, MPI_INT, i, TAG_TAMANHO_FILA_RESPOSTA, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
 
-        for (i = 0; i < NUMERO_CAIXAS; i++) {
-            if (caixas_fila[i] < caixas_fila[indexMenorFila]) {
-                indexMenorFila = i;
+        if (tamanho_filas[menor] > tamanho_filas[i]) {
+            menor = i;
+        }
+    }
+
+    return menor;
+}
+
+void produtor() {
+    int cliente = 0, i;
+    int nulo = -1;
+    int index_menor_fila;
+    while (1) {
+        cliente = cria_cliente();
+        
+        if (cliente == -1) {
+            break;
+        }
+
+        index_menor_fila = menor_fila();
+
+        for (i = 1; i < NUMERO_CAIXAS; i++) {
+            if (i != index_menor_fila) {
+                MPI_Send(&nulo, 1, MPI_INT, i, TAG_ATENDIMENTO, MPI_COMM_WORLD);
+            } else {
+                printf("Serão adicionados %d clientes em %d\n", cliente, i);
+                MPI_Send(&cliente, 1, MPI_INT, index_menor_fila, TAG_ATENDIMENTO, MPI_COMM_WORLD);
             }
-        }
-        caixas_fila[indexMenorFila]++;
-
-        for (i = 0; i < NUMERO_CAIXAS; i++) {
-            if (caixas_fila[i] > 0) {
-                printf("Cliente inserido no caixa %i\n", i + 1);
-                MPI_Send(&valor, 1, MPI_INT, i + 1, TAG_CAIXA_SND, MPI_COMM_WORLD);
-                numero_cliente++;
-            }
-        }
-
-        for (i = 0; i < NUMERO_CAIXAS; i++) {
-            if (caixas_fila[i] > 0) {
-                MPI_Recv(&ack, 1, MPI_INT, i + 1, TAG_CAIXA_RCV, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-                printf("Cliente atendido com sucesso no caixa %i\n", i + 1);
-                caixas_fila[i]--;
-            }
-        }
-
-        for (i = 0; i < NUMERO_CAIXAS; i++) {
-            soma_filas += caixas_fila[i];
-        }
-        if (soma_filas <= 0) {
-            MPI_Finalize();
         }
     }
 }
 
 void consumidor(int rank) {
-    int cliente_atendido = 0;
-    int ack = 1;
+    int fila = 0;
+    int i, m, flag = 1;
+    int resposta_tamanho_fila = 1;
+
     while (1) {
-        MPI_Recv(&cliente_atendido, 1, MPI_INT, 0, TAG_CAIXA_SND, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-        sleep(1);
-        MPI_Send(&ack, 1, MPI_INT, 0, TAG_CAIXA_RCV, MPI_COMM_WORLD);
+        MPI_Recv(&resposta_tamanho_fila, 1, MPI_INT, 0, TAG_TAMANHO_FILA_PEDIDO, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+        MPI_Send(&fila, 1, MPI_INT, 0, TAG_TAMANHO_FILA_RESPOSTA, MPI_COMM_WORLD);
+
+        MPI_Recv(&m, 1, MPI_INT, 0, TAG_ATENDIMENTO, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+
+        if (m != -1) {
+            fila += m;
+        }
+        if(fila > 0){
+            atende_cliente(rank);
+            fila--;
+        }
+        
     }
 }
 
 int main(int argc, char** argv) {
-
+    srand( (unsigned)time(NULL) );
     MPI_Init(&argc, &argv);
 
     int rank;
